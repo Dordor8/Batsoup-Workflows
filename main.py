4a4a4de039944ca8cbfd5e07f6d325805d9fb247
Dordor8 <farfarador@gmail.com>
2026-03-13 11:57:30 +0200

    yaml parser is working

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from src.yaml_parser import get_template
+from src.yaml_parser import get_template, get_task, get_workflow, dump_yaml
+from ruamel.yaml import YAML
 
+yaml = YAML()
+data = get_template({'tests': 't/tests.txt', 'test2': 't/test2.txt'},'my-temp','image:version','python','main.py',{'v': 'rr', 'v2': 'tt'},{'test3': 't/test3.txt'})
+data2 = get_task('task1', 'kafka-reader', ['task0', 'taska'], '{{tasks.task0.outputs.results}} == true')
+data3 = get_task('task2', 'kafka-reader', ['task0', 'taska'], '{{tasks.task0.outputs.results}} == true')
 
-print(get_template({'test': 't/test.txt', 'test2': 't/test2.txt'},'my-temp','image:version','python','main.py',{'v': 'rr', 'v2': 'tt'},{'test3': 't/test3.txt'}))
+workflow = get_workflow('my-workflow', [data2, data3], [data])
+
+dump_yaml(workflow, './tests/test-workflow.yaml')
--- src/yaml_parser.py
+++ src/yaml_parser.py
@@ -1,10 +1,10 @@
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString
-import os
 
-TEMPLATE_PATH = './resources/template-template.yaml'
-TASK_PATH = './resources/task-template.yaml'
-WORKFLOW_PATH = './resources/workflow-template.yaml'
+TEMPLATE_PATH = './resources/templates/template-template.yaml'
+TASK_PATH = './resources/templates/task-template.yaml'
+WORKFLOW_PATH = './resources/templates/workflow-template.yaml'
+MAIN_TEMPLATE_NAME = 'main'
 
 yaml = YAML()
 
@@ -19,8 +19,6 @@ def get_template(input_artifacts: dict[str, str],
     with open(TEMPLATE_PATH, "r") as template_file:
         content = yaml.load(template_file)
 
-    print(content)
-
     content['inputs']['artifacts'] = [{'name': key, 'path': value} for key, value in input_artifacts.items()]
     content['name'] = name
     content['container']['image'] = image
@@ -29,7 +27,35 @@ def get_template(input_artifacts: dict[str, str],
     content['container']['env'] = [{'name': key, 'value': DoubleQuotedScalarString(value)} for key, value in env_variables.items()]
     content['outputs']['artifacts'] = [{'name': key, 'path': value} for key, value in output_artifacts.items()]
 
-    with open("./test/test-template.yaml", "w") as test:
-        yaml.dump(content, test)
+    return content
+
+def get_task(name: str, template: str, dependencies: list[str], when: str) -> dict:
+    with open(TASK_PATH, "r") as task_file:
+        content = yaml.load(task_file)
+
+    content['name'] = name
+    content['template'] = template
+    content['dependencies'] = [DoubleQuotedScalarString(dependency) for dependency in dependencies]
+    content['when'] = DoubleQuotedScalarString(when)
 
     return content
+
+def get_workflow(name: str, tasks: list[dict], templates: list[dict]) -> dict:
+    with open(WORKFLOW_PATH, "r") as workflow_file:
+        content = yaml.load(workflow_file)
+
+    content['metadata']['generateName'] = name + '-'
+    for template in templates:
+        content['spec']['templates'].append(template)
+
+    for template in content['spec']['templates']:
+        if template['name'] == MAIN_TEMPLATE_NAME:
+            for task in tasks:
+                template['dag']['tasks'].append(task)
+            break
+
+    return content
+
+def dump_yaml(content: dict[str, str], path: str):
+    with open(path, "w") as yaml_file:
+        yaml.dump(content, yaml_file)
